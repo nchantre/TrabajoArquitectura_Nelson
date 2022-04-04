debc75f8b91bdd125185d58fba742dce74ae11c7
nchantre <nelsonfabiancs@hotmail.com>
2022-04-03 22:04:42 -0500

    Proyecto Realizado en Php
    Arquitectura en capas y
    y scrip de bd

---
+++ Proyectoparquedero/Models/ClienteModel.php
@@ -0,0 +1,13 @@
+<?php
+
+class ClienteModel {
+    private $_identificacion;
+    private $_nombre;
+    private $_apellidos;
+    
+    public function getIdentificacion() {
+        return $this->_identificacion;
+        
+    }
+    //put your code here
+}
---
+++ Proyectoparquedero/view/ViewLogic/ClientesViewLogic.php
@@ -0,0 +1,14 @@
+<?php
+
+$identificacion = $_POST['identificacion'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+echo '<br>';
+echo 'el numero de identifcacion registrado es :',$identificacion;
+echo '<br>';
+echo 'el nombre registrado es',$nombre;
+echo '<br>';
+echo 'el apellido registrado es:',$apellido;
+
+
+?>
